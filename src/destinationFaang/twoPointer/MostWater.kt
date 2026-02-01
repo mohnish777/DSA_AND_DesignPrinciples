702c259678823eb27a577a49ca35806df2d613e9
mohnish777 <cmohniskumar1205@gmail.com>
2026-02-01 18:04:59 +0530

    Most water sliding window algo

--- src/destinationFaang/twoPointer/MostWater.kt
+++ src/destinationFaang/twoPointer/MostWater.kt
@@ -1,12 +1,27 @@
 package destinationFaang.twoPointer
 
+// Leet code - 11. Container With Most Water
+// 6hrs
 class MostWater {
-    fun mostWater() {
-        
+    fun maxArea(height: IntArray): Int {
+        var left = 0
+        var right = height.size - 1
+        var maxArea = 0
+        while (left < right) {
+            val width = right - left
+            if (height[left] > height[right]){
+                maxArea = maxOf(maxArea, height[right]*width)
+                right--
+            } else {
+                maxArea = maxOf(maxArea, height[left]*width)
+                left ++
+            }
+        }
+        return maxArea
     }
 }
 
 
 fun main() {
-    println(twoSum(intArrayOf(2,7,11,15), 9))
+    println(MostWater().maxArea(intArrayOf(1,8,6,2,5,4,8,3,7)))
 }
