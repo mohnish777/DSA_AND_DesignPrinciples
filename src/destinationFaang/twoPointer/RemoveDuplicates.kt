c0cd0a7b234e3c832a4bcbd802eb98d67b2767c5
mohnish777 <cmohniskumar1205@gmail.com>
2026-02-02 23:38:48 +0530

    remove duplicate two pointer algo

--- src/destinationFaang/twoPointer/RemoveDuplicates.kt
+++ src/destinationFaang/twoPointer/RemoveDuplicates.kt
@@ -3,8 +3,22 @@ package destinationFaang.twoPointer
 // Leet code 26
 // 6hrs : 30 mins
 class RemoveDuplicates {
-    fun removeDuplicates(nums: IntArray) {
-
+    fun removeDuplicates(nums: IntArray): Int {
+        var index = 1
+        for ( i in 1 until nums.size) {
+            if(nums[i-1] !=nums[i]) {
+                nums[index] = nums[i]
+                index++
+            }
+        }
+        return index
     }
+}
 
+
+fun main() {
+    val removeDuplicates = RemoveDuplicates()
+    println(removeDuplicates.removeDuplicates(intArrayOf(1,2,3,4,5)))
+    println(removeDuplicates.removeDuplicates(intArrayOf(1,2,3,4,5)))
+    println(removeDuplicates.removeDuplicates(intArrayOf(1,1,2)))
 }
