da09f0e593effcedb0582dc004109c158c9ed0bf
mohnish777 <cmohniskumar1205@gmail.com>
2026-02-08 12:54:48 +0530

    longest common prefix algo

--- src/destinationFaang/strings/LongestCommonPrefix.kt
+++ src/destinationFaang/strings/LongestCommonPrefix.kt
@@ -3,11 +3,20 @@ package destinationFaang.strings
 // Leetcode 14 - 7hrs :20 mins
 class LongestCommonPrefix {
     fun longestCommonPrefix(strs: Array<String>): String {
+        if (strs.isEmpty()) return ""
+        var prefix = ""
+        for (i in 0 until strs.size) {
+            while (!strs[i].startsWith(prefix)) {
+                prefix =  prefix.substring(0, prefix.length -1)
 
+                if (prefix.isEmpty()) return ""
+            }
+        }
+        return prefix
     }
 }
 
 
 fun main() {
-    
+
 }
