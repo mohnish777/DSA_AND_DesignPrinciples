22ad9fece703d6281ae10d1d6bafa783e8e61cbf
mohnish777 <cmohniskumar1205@gmail.com>
2026-06-15 23:42:09 +0530

    add time and space complexity

--- src/destinationFaang/trees/sameTree.kt
+++ src/destinationFaang/trees/sameTree.kt
@@ -20,3 +20,30 @@ class sameTree {
         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
     }
 }
+
+/*
+*
+* LeetCode 100 - Same Tree Complexity
+
+n = number of nodes
+h = height of tree
+
+Time:
+O(n)
+Reason: In worst case, visit every node once.
+
+Space:
+O(h)
+Reason: recursion call stack depends on tree height.
+
+Balanced tree:
+h = log n, so space = O(log n)
+
+Skewed tree:
+h = n, so space = O(n)
+
+General answer:
+Time:  O(n)
+Space: O(h)
+*
+* */
